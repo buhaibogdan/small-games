89afd653e9eeeb1089330f48a59745c5458c4e5e
Bogdan Buhai <buhaibogdan@user.noreply.github.com>
2014-08-30 23:28:04 +0300

    Minesweeper game progress

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Planes Game' })
+    res.render('index', { title: 'Planes Game' });
+};
+
+exports.mine = function(req, res) {
+    res.render('mine', {title: 'minesweeper'});
 };
